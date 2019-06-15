3be484779e1b1894728436953f3367c2c9c9b9a0
José Ítalo <italo_alvees@live.com>
2019-06-15 19:14:56 -0300

    commitando

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,27 +4,46 @@ import 'screens_components.dart';
 class LoginScreen extends StatelessWidget with ScreensComponents{
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
+
     return Scaffold(
       backgroundColor: customColorLogin.mainColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top: 25, bottom: 70),
-            child: CircleAvatar(
-              radius: 70,
-              backgroundColor: customColorLogin.white,
-              child: Text("Logo",
-                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: customColorLogin.mainColor) ,),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(bottom: 20),
-            child: campo("Login"),
-          ),
-          campo("Senha")
-        ],
-      ),
-    );
+      body: Center(
+        child: ListView(
+          shrinkWrap: true  ,
+          children: <Widget>[
+            Form(
+              key: formKey,
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                //LOGO DO APP
+                Container(
+                  padding: EdgeInsets.only(bottom: 70),
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundColor: customColorLogin.white,
+                    child: Text("Logo",
+                      style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: customColorLogin.mainColor) ,),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: 25),
+                  //TEXTFIELD DE LOGIN
+                  child: campo("Login"),
+                ),
+                Container(
+                  //TEXTFIELD DE SENHA
+                  child: campo("Senha"),
+                  padding: EdgeInsets.only(bottom: 50),
+                ),
+                //BOTÃO DE LOGIN
+                botao()
+              ],
+            ),)
+          ],
+        ),
+      )
+      );
   }
 }
--- lib/screens/screens_components.dart
+++ lib/screens/screens_components.dart
@@ -9,17 +9,30 @@ mixin ScreensComponents{
   Widget campo(String nome, { var validator, Function function} ){
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 48),
+      height: 49,
       color: customColorLogin.white,
       child: TextFormField(
-        style: TextStyle(color: customColorLogin.mainColor),
+        style: TextStyle(color: customColorLogin.mainColor, fontWeight: FontWeight.w400),
         decoration: InputDecoration(
             hintText: nome,
-            hintStyle: TextStyle(color: customColorLogin.mainColor, fontWeight: FontWeight.w400),
+            hintStyle: TextStyle(height: -1,fontSize: 17, color: customColorLogin.mainColor, fontWeight: FontWeight.w400),
             border: OutlineInputBorder()
         ),
       ),
     );
 
+  }
+
+  Widget botao(){
+    return InkWell(
+      child: Container(
+        alignment: Alignment.center,
+        height: 49,
+        margin: EdgeInsets.symmetric(horizontal: 49),
+        color: customColorLogin.white,
+        child: Text("Login", style: TextStyle(fontSize: 17, color: customColorLogin.mainColor, fontWeight: FontWeight.w600),)
+      )
+    );
   }
 
 }
