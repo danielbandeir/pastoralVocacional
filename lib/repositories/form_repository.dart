7214eaf44251b15f0e525d85599cfa3149578dd4
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-06-15 20:37:43 -0300

    Added abrastract form repostiroy form

--- lib/repositories/form_repository.dart
+++ lib/repositories/form_repository.dart
@@ -1,3 +1,7 @@
+import 'package:pastoravocacional/models/form_vocacional.dart';
+
 abstract class FormRepository{
-  
+  Future<void> createForm(FormVocacional formVocacional);
+  Future<void> updateForm(FormVocacional formVocacional);
+  Future<void> deleteForm(FormVocacional formVocacional);
 }
