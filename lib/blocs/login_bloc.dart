83d8ab0c5548ce450bf41cb156d0a767f2d7657a
Paulo Henrique <paulohbmatias@cc.ci.ufpb.br>
2019-06-15 17:30:29 -0300

    adicionado validaçao no botao

--- lib/blocs/login_bloc.dart
+++ lib/blocs/login_bloc.dart
@@ -1,23 +1,26 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:pastoravocacional/blocs/login_validators.dart';
 import 'package:pastoravocacional/repositories/auth_respository.dart';
 import 'package:rxdart/rxdart.dart';
 
-class LoginBloc extends BlocBase {
-  
+class LoginBloc extends BlocBase with LoginValidators {
   final AuthRepository authRepository;
 
-  BehaviorSubject<String> _email = BehaviorSubject(); 
+  BehaviorSubject<String> _email = BehaviorSubject();
   BehaviorSubject<String> _password = BehaviorSubject();
 
   LoginBloc(this.authRepository);
 
-  Observable<String> get email => _email.stream; 
-  Observable<String> get password => _password.stream;
+  Observable<String> get email => _email.stream.transform(validateEmail);
+  Observable<String> get password =>
+      _password.stream.transform(validatePassword);
+  Observable<bool> get submitValid =>
+      Observable.combineLatest2(email, password, (email, password) => true);
 
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
 
-  void submit(Function(String) callBackError) async{
+  void submit(Function(String) callBackError) async {
     try {
       await authRepository.signIn(_email.value, _password.value);
     } catch (e) {
@@ -25,11 +28,11 @@ class LoginBloc extends BlocBase {
     }
   }
 
-  void signOut(Function callBack) async{
-    try{
+  void signOut(Function callBack) async {
+    try {
       await authRepository.signOut();
       callBack();
-    }catch(e){
+    } catch (e) {
       print(e);
     }
   }
