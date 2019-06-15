66f8355d355d566b049c0e93424c5156b31dcd13
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-06-15 17:53:47 -0300

    Added methods to use

---
+++ lib/repositories/impl/event_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'package:pastoravocacional/repositories/event_repository.dart';
+import 'package:pastoravocacional/models/evento.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class EventRepositoryImpl implements EventRepository{
+
+  @override
+  Future<void> createEvent(Evento event) async{
+    await Firestore.instance
+                .collection('eventos')
+                .document(event.local)
+                .setData({'evento' : event});
+  }
+
+  @override
+  Future<void> updateEvent(Evento event) async{
+    await Firestore.instance
+                .collection('eventos')
+                .document(event.local)
+                .updateData({'evento' : event});
+  }
+
+  @override
+  Future<void> deleteEvent(Evento event) async{
+    await Firestore.instance
+                .collection('eventos')
+                .document(event.local)
+                .delete();
+  }
+
+}
