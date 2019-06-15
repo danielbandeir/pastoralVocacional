c17a34426bc0a6a2ff543172959312522ea29759
Paulo Henrique <paulohbmatias@cc.ci.ufpb.br>
2019-06-15 17:14:04 -0300

    adicionado dependencias

--- lib/blocs/login_bloc.dart
+++ lib/blocs/login_bloc.dart
@@ -1,10 +1,15 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:pastoravocacional/repositories/auth_respository.dart';
 import 'package:rxdart/rxdart.dart';
 
 class LoginBloc extends BlocBase {
   
+  final AuthRepository authRepository;
+
   BehaviorSubject<String> _email = BehaviorSubject(); 
   BehaviorSubject<String> _password = BehaviorSubject();
+
+  LoginBloc(this.authRepository);
 
   Observable<String> get email => _email.stream; 
   Observable<String> get password => _password.stream;
@@ -12,9 +17,9 @@ class LoginBloc extends BlocBase {
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
 
-  void submit(Function(String) callBackError){
+  void submit(Function(String) callBackError) async{
     try {
-      //TODO
+      await authRepository.signIn(_email.value, _password.value);
     } catch (e) {
       callBackError("Erro ao realizar login");
     }
---
+++ lib/repositories/impl/auth_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:pastoravocacional/repositories/auth_respository.dart';
+
+class AuthRepositoryImpl implements AuthRepository{
+  @override
+  Future<void> signIn(String email, String senha) {
+    // TODO: implement signIn
+    return null;
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    return null;
+  }
+  
+}
