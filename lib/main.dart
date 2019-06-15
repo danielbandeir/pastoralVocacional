f1041ceafe0e37e36287707c41203d732f96fdc3
José Ítalo <italo_alvees@live.com>
2019-06-15 17:19:01 -0300

    Começando os trabalhos

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,17 @@
-import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'screens/login_screen.dart';
-=======
-import 'package:pastoravocacional/blocs/login_bloc.dart';
-import 'package:pastoravocacional/repositories/auth_respository.dart';
-import 'package:pastoravocacional/repositories/impl/auth_repository_impl.dart';
->>>>>>> 00a6ad3b96ac2cc87356eba513b26671d8986c84
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      blocs: [
-        Bloc((i) => LoginBloc(i.get<AuthRepository>()))
-      ],
-      dependencies: [
-        Dependency((i) => AuthRepositoryImpl()),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Container(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-<<<<<<< HEAD
       home: LoginScreen(),
-=======
->>>>>>> 00a6ad3b96ac2cc87356eba513b26671d8986c84
     );
   }
 }
