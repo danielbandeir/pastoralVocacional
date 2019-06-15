b9518d1904435b54f584ebaa90c661613ee70391
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-06-15 20:37:56 -0300

    Added to form repository implements

---
+++ lib/repositories/impl/form_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:pastoravocacional/models/form_vocacional.dart';
+import 'package:pastoravocacional/repositories/form_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FormRepositoryImpl implements FormRepository{
+  @override
+  Future<void> createForm(FormVocacional formVocacional){
+    Firestore.instance
+                .collection('formularios')
+                .document(formVocacional.integrante.nome)
+                .setData({'formulario' : formVocacional});
+  }
+
+  @override
+  Future<void> updateForm(FormVocacional formVocacional){
+    Firestore.instance
+                .collection('formularios')
+                .document(formVocacional.integrante.nome)
+                .updateData({'formulario' : formVocacional});
+  }
+
+  @override
+  Future<void> deleteForm(FormVocacional formVocacional){
+    Firestore.instance
+                .collection('formularios')
+                .document(formVocacional.integrante.nome)
+                .delete();
+  }
+}
