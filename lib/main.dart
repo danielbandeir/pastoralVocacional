acb9948468597327098abcb94b2ca9737d0fb807
Paulo Henrique <paulohbmatias@cc.ci.ufpb.br>
2019-06-15 17:21:34 -0300

    corrigido home fora do provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'screens/login_screen.dart';
-=======
 import 'package:pastoravocacional/blocs/login_bloc.dart';
 import 'package:pastoravocacional/repositories/auth_respository.dart';
 import 'package:pastoravocacional/repositories/impl/auth_repository_impl.dart';
->>>>>>> 00a6ad3b96ac2cc87356eba513b26671d8986c84
+import 'screens/login_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,12 +22,8 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Container(),
+        home: LoginScreen(),
       ),
-<<<<<<< HEAD
-      home: LoginScreen(),
-=======
->>>>>>> 00a6ad3b96ac2cc87356eba513b26671d8986c84
     );
   }
 }
