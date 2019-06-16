b78a417c6f6c06a03daf5ad403c3205006c46fba
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-06-15 21:02:54 -0300

    Changed event form

--- lib/models/evento.dart
+++ lib/models/evento.dart
@@ -3,14 +3,17 @@ import 'dart:collection';
 class Evento {
   String data;
   String local;
+  String eventType;
 
   Evento.fromJSON(LinkedHashMap<String, dynamic> map){
     this.data = map['data'];
     this.local = map['local'];
+    this.eventType = map['tipoEvento'];
   }  
 
   Map<String, dynamic> toMap() => {
     'data': this.data,
-    'local': this.local
+    'local': this.local,
+    'tipoEvento' : this.eventType
   };
 }
