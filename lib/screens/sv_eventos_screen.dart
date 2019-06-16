3830d2cf9bfc149c6e836aa9246382555f9a1416
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-06-16 02:52:05 -0300

    Changed tab

---
+++ lib/screens/sv_eventos_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SVEventosScreen extends StatefulWidget {
+  @override
+  _SVEventosScreenState createState() => _SVEventosScreenState();
+}
+
+class _SVEventosScreenState extends State<SVEventosScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
